c04009de257082f9a5b6f0db3e9d4064e3f1cc88
Arinu <abj5267@gmail.com>
2016-12-06 00:59:24 +0800

    update Sokoban

--- Game/src/components/Sokoban.vue
+++ Game/src/components/Sokoban.vue
@@ -2,7 +2,7 @@
  <div class="Sokoban">
    <h1>推箱子</h1>
    <p><button type="button" @click="reset">重新来过</button></p>
-   <!--<button>下一关</button>-->
+   <button @click="next">下一关</button>
    <ul>
      <li
       v-for="item in present"
@@ -28,10 +28,10 @@
                 0,0,0,0,0,0,0,0,0,
                 0,0,0,3,0,0,0,0,0,
                 0,0,0,1,0,0,0,0,0,
-                0,0,0,2,1,2,2,3,0,
+                0,0,0,2,1,2,1,3,0,
                 0,3,1,2,4,0,0,0,0,
                 0,0,0,0,2,0,0,0,0,
-                0,0,0,0,2,0,0,0,0,
+                0,0,0,0,1,0,0,0,0,
                 0,0,0,0,3,0,0,0,0,
                 0,0,0,0,0,0,0,0,0],
         leve2: [
@@ -54,52 +54,32 @@
     },
     methods: {
       reset(){
-        
+        this.present = eval('this.leve'+this.now).slice(0)
       },
-      moveLeft(){
-        // const m = -1
-        // const me = this.present.indexOf(4)
-        // const n = this.pass.indexOf(me)
-        // console.log(me)
-        // switch(this.present[me+1*m]){
-        //   case 1 : case 3:
-        //     const s = n===-1?this.present[me+1*m]:3
-        //     this.present.splice(me+1*m,1,4)
-        //     this.present.splice(me,1,s)
-        //     console.log(n)
-        //   break;
-        //   case 2:
-        //     switch(this.present[me+2*m]){
-        //       case 1: case 3: 
-        //       const k = n===-1?1:3
-        //       this.present.splice(me+2*m,3,2,4,1)
-        //     }
-        //   break;
-        // }
-        this.move(-1)
+      next(){
+        this.now++
+        this.present = eval('this.leve'+this.now).slice(0)
+        // this.eval('leve'+this.now)
+        this.pass = []
+        for(let i=0,g=this.present.length; i<g; i++){
+          this.present[i] === 3 && this.pass.push(i)
+        }
       },
-      moveTop(){
-        const x = Math.sqrt(this.present.length)
-        this.move(-x)
-      },
-      moveRight(){
-        this.move(+1)
-      },
-      moveDown(){
-        const x = Math.sqrt(this.present.length)
-        this.move(+x)
+      evil(ff){
+        let Fn = Function
+        // console.log(this.now)
+        const f = new Fn('console.log(this);return '+ 'this.'+ff)
+        return f()
       },
       move(m){
         let pass_v = 0
         const me = this.present.indexOf(4)
-        const n = this.pass.indexOf(me)
-        console.log(me)
+        const n = this.pass.indexOf(me)//移动前位置是否是坑
         switch(this.present[me+1*m]){
           case 1 : case 3:
-            const s = n === -1 ? this.present[me+1*m] : 3
+            const s = n === -1 ? 1 : 3
             this.present.splice(me+1*m,1,4)
             this.present.splice(me,1,s)
-            console.log(n)
           break;
           case 2:
             switch(this.present[me+2*m]){
@@ -113,7 +93,6 @@
         }
         for(var item in this.pass){
           this.present[this.pass[item]]===2 && pass_v++
-          console.log('a: '+this.pass[item])
         }
         if(pass_v===this.pass.length){
           setTimeout(function(){
@@ -123,19 +102,20 @@
       }
     },
     created (){
-      console.log(this.present);
-      console.log(this.leve1.slice(0))
+      // console.log(this.present);
+      // console.log(this.leve1.slice(0))
       // 初始化
-      this.present = this.leve2.slice(0)
+      this.present = this.leve1.slice(0)
       // 记录当前关卡通关条件
       for(let i=0,g=this.present.length; i<g; i++){
         this.present[i] === 3 && this.pass.push(i)
       }
       document.onkeydown = function(e){
-        e.keyCode===37 && this.moveLeft()
-        e.keyCode===38 && this.moveTop()
-        e.keyCode===39 && this.moveRight()
-        e.keyCode===40 && this.moveDown()
+        const x = Math.sqrt(this.present.length)
+        e.keyCode===37 && this.move(-1)
+        e.keyCode===38 && this.move(-x)
+        e.keyCode===39 && this.move(+1)
+        e.keyCode===40 && this.move(+x)
       }.bind(this)
     }
   })
